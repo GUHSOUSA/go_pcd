816df121f7eb8c4bbccf02b8979eed0a80b1aacd
gustavosousagh <gustavo.sousa@ilegra.com>
2025-01-11 16:07:45 -0300

    Remover .env do versionamento

--- cmd/main.go
+++ cmd/main.go
@@ -8,9 +8,16 @@ import (
 
 	"github.com/gustasousaghs/carros-pcd-service/config"
 	"github.com/gustasousaghs/carros-pcd-service/router"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if os.Getenv("APP_ENV") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Println("Arquivo .env não encontrado, continuando com variáveis de ambiente do sistema")
+		}
+	}
 	config.MysqlConnect()
 
 	port := os.Getenv("PORT") // Railway define essa variável automaticamente
